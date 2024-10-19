5d493aec2b49c7fca24c28330e6e72144340d103
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-19 16:54:00 +0300

    2d arrays

--- src/Arrays/ArrayBasics.java
+++ src/Arrays/ArrayBasics.java
@@ -3,7 +3,7 @@ package Arrays;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class arrays {
+public class ArrayBasics {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
---
+++ src/Arrays/MultiDimension.java
@@ -0,0 +1,78 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MultiDimension {
+    public static void main(String[] args) {
+        // Quick note: This declaration is valid
+        int[][] arr2d = {
+                {1, 2, 3},    // arr2d[0]
+                {4, 5},       // arr2d[1]
+                {6, 7, 8, 9}  // arr2d[2]
+        };
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the array (3x2) elements");
+
+        int[][] arr = new int[3][2];
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = in.nextInt();
+            }
+        }
+        // in: 1 2 3 4 5 6 7 8
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+        /*
+            out:  1 2
+                  3 4
+                  5 6
+         */
+
+        System.out.println();
+
+        // Example to output an irregular multidimensional array
+        int[][] myArr = {
+                {1, 2, 3},
+                {4, 5},
+                {6, 7, 8, 9}
+        };
+
+        for (int row = 0; row < myArr.length; row++) {
+            for (int col = 0; col < myArr[row].length; col++) {
+                System.out.print(myArr[row][col] + " ");
+            }
+            System.out.println();
+        }
+        /*
+             out:  1 2 3
+                   4 5
+                   6 7 8 9
+         */
+        System.out.println();
+
+        // Outputting a 2D array in shorter hand using Arrays.toString() method
+        int[][] xarr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        // We can even make it shorter using enhanced for loop
+        for (int[] a : xarr) {
+            System.out.println(Arrays.toString(a));
+        }
+        /*
+              out:  [1, 2, 3]
+                    [4, 5, 6]
+                    [7, 8, 9]
+         */
+    }
+}
