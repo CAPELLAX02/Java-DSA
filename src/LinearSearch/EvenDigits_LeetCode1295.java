09b2a6080f3ae40da4fb6d080cf30a38ebf353f4
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-20 14:29:11 +0300

    Leetcode - 1295 solution

---
+++ src/LinearSearch/EvenDigits_LeetCode1295.java
@@ -0,0 +1,48 @@
+package LinearSearch;
+
+public class EvenDigits_LeetCode1295 {
+    public static void main(String[] args) {
+        int[] nums = { 12, 345, 2, 6, 7896 };
+        System.out.println(findNumbers(nums));
+        // out: 2
+
+        int[] nums2 = { 0, -20, -300, 4000, 50000, 99 };
+        System.out.println(findNumbers(nums2));
+        // out: 3
+    }
+
+    // Main solution function
+    static int findNumbers(int[] nums) {
+        int count = 0;
+
+        for (int num : nums) {
+            if (even(num)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    // function to check if a number contains even digits
+    static boolean even(int num) {
+        return digits(num) % 2 == 0;
+    }
+
+    // count number of digits in a number
+    static int digits(int num) {
+        if (num < 0) num *= -1;
+
+        if (num == 0) return 1;
+
+        int count = 0;
+
+        while (num > 0) {
+            count++;
+            num /= 10;
+        }
+
+        return count;
+    }
+
+}
